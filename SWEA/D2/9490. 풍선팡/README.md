040e7303072e142128487e17ce250251aeaaa5ec
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2024-02-01 14:02:00 +0900

    [D2] Title: 풍선팡, Time: 333 ms, Memory: 94,228 KB -BaekjoonHub

---
+++ SWEA/D2/9490. 풍선팡/README.md
@@ -0,0 +1,15 @@
+# [D2] 풍선팡 - 9490 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXAerAPaVXMDFARP) 
+
+### 성능 요약
+
+메모리: 94,228 KB, 시간: 333 ms, 코드길이: 801 Bytes
+
+### 제출 일자
+
+2024-02-01 14:01
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/9490. 풍선팡/풍선팡.py
@@ -0,0 +1,24 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    N, M = list(map(int, input().split()))
+    maps = [list(map(int, input().split())) for _ in range(N)]
+
+    flower_list = []
+    for i in range(N):
+        for j in range(M):
+            bomb_num = maps[i][j]
+            basic_flower_sum = maps[i][j]
+            for p in range(1, bomb_num+1):
+                if j+p <M:
+                    basic_flower_sum += maps[i][j+p]
+                if j-p >= 0:
+                    basic_flower_sum += maps[i][j-p]
+                if i+p < N:
+                    basic_flower_sum += maps[i+p][j]
+                if i-p >= 0:
+                    basic_flower_sum += maps[i-p][j]
+                flower_list.append(basic_flower_sum)
+
+    max_flower = max(flower_list)
+
+    print(f'#{test_case} {max_flower}')
