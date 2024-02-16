5b143e9795652974dae26cffe00fd0c0d6fde054
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2024-02-16 14:26:39 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 162 ms, Memory: 55,820 KB -BaekjoonHub

---
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+move = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+def bfs():
+    global q
+
+    while q:
+        temp = []
+        while q:
+            x, y = q.pop()
+            for i, j in move:
+                dx = x+i
+                dy = y+j
+                if 0 <= dx < 16 and 0 <= dy < 16:
+                    if not maps[dx][dy]:
+                        maps[dx][dy] = 1
+                        temp.append((dx, dy))
+                    if maps[dx][dy] == 3:
+                        return 1
+        q = temp
+    return 0
+
+T = 10
+for test_case in range(1, T + 1):
+    tc = int(input())
+    maps = [list(map(int, input().strip())) for _ in range(16)]
+
+    q = deque()
+    q.extend([(1, 1)])
+
+    print(f'#{test_case} {bfs()}')
