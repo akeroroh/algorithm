5afdeec5f907cbcfae184a18c8bdc5b211897543
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2024-02-26 16:41:33 +0900

    [D3] Title: 원 안의 점, Time: 284 ms, Memory: 57,980 KB -BaekjoonHub

--- SWEA/D3/16910. 원 안의 점/원 안의 점.py
+++ SWEA/D3/16910. 원 안의 점/원 안의 점.py
@@ -1,11 +1,10 @@
 T = int(input())
-for test_case in range(1, T + 1):
-    num = int(input())
+for test_case in range(1, T+1):
+    N = int(input())
 
-    num_count = 0
-    for i in range(-num, num+1):
-        for j in range(-num, num+1):
-            if i**2 + j**2 <= num**2:
-                num_count += 1
-
-    print(f'#{test_case} {num_count}')
+    count = 0
+    for i in range(1, N):
+        for j in range(1, N):
+            if i**2 + j**2 <= N**2:
+                count += 1
+    print(f'#{test_case} {count*4+(N*4)+1}')
